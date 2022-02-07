8babb644b462aaf4cc5a94973725f4481dfb4f4a
Teo <teo.hansson@jgy.se>
2022-02-07 14:30:49 +0100

    Damage and Health Reduction
    Enemies now damage the Storpingvin when they collide. If the Storpingvin's lives are 0, it'll send a debug log with the message "Game Over".

--- Penguin Tower Defense/Assets/Enemy.cs
+++ Penguin Tower Defense/Assets/Enemy.cs
@@ -5,24 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     
-    public void OnTriggerEnter2D(Collider2D collision)
-    {
-        var storpingvin = collision.GetComponent<Storpingvin>();
-        if (storpingvin)
-        {
-            Debug.Log("has collide");
-            Destroy(collision.gameObject);
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
