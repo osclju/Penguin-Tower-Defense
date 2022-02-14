b2ac6cffbd50a5c054f4fd2f02140de3c11284c1
cowser <pontus.dadell@bredband.net>
2022-02-14 12:23:03 +0100

    Level select
    Corrected sizing

--- Penguin Tower Defense/Assets/Scrips/MenuScript.cs
+++ Penguin Tower Defense/Assets/Scrips/MenuScript.cs
@@ -1,17 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float ContollX, ContollY;
+
+    public Sprite newImage;
+    private Image myIMGcomponent;
+
     void Start()
     {
-        
+        myIMGcomponent = this.GetComponent<Image>();
+        myIMGcomponent.sprite = newImage;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        ContollX = Input.GetAxisRaw("Horizontal");
+        ContollY = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        if (ContollX == -1) //Left
+        {
+            Debug.Log("left");
+        }
+        else if (ContollX == 1) //Right
+        {
+            Debug.Log("Right");
+        }
+        else if (ContollY == 1) //UP
+        {
+            Debug.Log("UP");
+        }
+        else if (ContollY == -1) //Down
+        {
+            Debug.Log("NEd");
+        }
+    }
+    
+    public void SelectLevel_picture_left()
     {
         
     }
