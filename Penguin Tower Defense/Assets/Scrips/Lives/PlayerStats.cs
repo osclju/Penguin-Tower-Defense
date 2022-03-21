552e9680a048a6bc0636f8e7fe2f0ab7c49cf8d2
Teo <teo.hansson@jgy.se>
2022-03-21 12:05:28 +0100

    ScriptableObjects for EnemyStats
    ScriptableObjects storing stats can now be created. Each enemy will need a scriptable object attached to their EnemyModel in order to contain stats.

--- Penguin Tower Defense/Assets/Scrips/Lives/PlayerStats.cs
+++ Penguin Tower Defense/Assets/Scrips/Lives/PlayerStats.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-    [SerializeField] private static int m_PlayerLives;
+    [SerializeField] private static int playerLives;
     [SerializeField] private int startLives = 20; 
 
 
     void Start()
     {
-        m_PlayerLives = startLives;
+        playerLives = startLives;
     }
 
     public static int PlayerLives
     {
-        get { return m_PlayerLives; }
-        set { m_PlayerLives = value; }
+        get { return playerLives; }
+        set { playerLives = value; }
     }
     
 
