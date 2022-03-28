83c51357f2138bbe220b4f243f77b27e1ec4dbf9
Elliot Karlsson <elgen2525@hotmail.com>
2022-03-28 12:24:39 +0200

    Now ships die by bullets
    immunitys and supereffetcivnes also works

--- Penguin Tower Defense/Assets/Scrips/isDead.cs
+++ Penguin Tower Defense/Assets/Scrips/isDead.cs
@@ -2,24 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class isDead : MonoBehaviour
 {
   
     [SerializeField] public int hp;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    // Kan göra listor av dessa så att skeppen har flera effective och immuninty
+    [SerializeField] public int immunity;
+    [SerializeField] public int superEffective;
+    
     // Update is called once per frame
     void Update()
     {
         if(hp <= 0)
         {
-            
             Debug.Log("DEAD");
             Destroy(gameObject);
         }
--- Penguin Tower Defense/Assets/game/shooting/scripts/collision.cs
+++ Penguin Tower Defense/Assets/game/shooting/scripts/collision.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class collision : MonoBehaviour
 {
-    int dmg, affect; 
-   
+    int dmg, affect;
+    private isDead ship;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "pirate") {
             dmg = gameObject.GetComponent<bulletVar>().dmg;
             affect = gameObject.GetComponent<bulletVar>().affect;
-            Debug.Log("Damage taken: " + dmg + " and affect: " + affect);
+            ship = collision.gameObject.GetComponent<isDead>();
+
+            if (ship.immunity == affect) {
+                dmg = 0;
+            }
+            else if (ship.superEffective == affect) {
+                dmg *= 2;
+            }
+            ship.hp -= dmg;
+            
+            Debug.Log("Damage taken: " + dmg + " and affect: " + affect + "HP left: " + ship.hp);
             Destroy(gameObject);
         }
     }
